0a51ee018042423d4222be48cd5fd1369346241b
nazimks <jm_keskes@esi.dz>
2025-02-23 16:58:14 +0100

    fixed merge conflits

---
+++ workspaces/extract_interview_questions/src/definitions_openai.py
@@ -0,0 +1,24 @@
+from dagster import Definitions
+
+from src.assets.text_processing_asset_openai import (
+    extract_entries,
+)
+from src.resources.openai_ressource import OpenAIResource
+from src.jobs_openai import process_all_pdfs
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+model_name = os.getenv("MODEL_NAME", "gpt-4o-mini")
+
+defs = Definitions(
+    assets=[
+        extract_entries,
+    ],
+    resources={
+        "openai_resource": OpenAIResource(model_name=model_name),
+    },
+    jobs=[
+        process_all_pdfs,
+    ],
+)
